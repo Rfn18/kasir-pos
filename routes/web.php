ec466dc5be2b3d6e63880f7fa1d9b70eade85fab
Rfn18 <rinofaster89@gmail.com>
2025-09-23 11:46:12 +0700

    update crud2

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Route::get('user/{id}/edit', [UserController::class, 'edit'])->name('users.edit'
 Route::delete('user/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
 Route::post('users', [UserController::class, 'store'])->name('users.store');
+
+Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
